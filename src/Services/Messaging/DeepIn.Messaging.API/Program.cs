fd1f7b6d6392083acff4273375d4ec524cd6ff36
Leo Yang <leoyang0105@gmail.com>
2023-08-06 18:31:11 +0800

    remove test code.

--- src/Services/Messaging/DeepIn.Messaging.API/Program.cs
+++ src/Services/Messaging/DeepIn.Messaging.API/Program.cs
@@ -7,10 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults(eventHandlerAssembly: typeof(Program).Assembly);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5009);
-});
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseServiceDefaults();
